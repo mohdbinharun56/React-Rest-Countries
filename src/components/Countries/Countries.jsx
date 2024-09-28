cba718ca16140f22656884b6ef379e513d0b1357
mohdbinharun56 <arahaman461@gmail.com>
2024-09-28 23:47:20 +0600

    add event handler 'onClick' to toggle with a button'

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,23 +1,25 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
-
+import './Countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res=>res.json())
-        .then((data)=>{
-            setCountries(data);
-            console.log(countries);
-        });
-    },[]);
+            .then(res => res.json())
+            .then((data) => {
+                setCountries(data);
+                console.log(countries);
+            });
+    }, []);
     return (
         <div>
-            <h1>Countries</h1>
-            {
-                countries.map(country=><Country country={country} key={country.cca2}></Country>)
-            } 
+            <h1>Countries: {countries.length}</h1>
+            <div className="countries-container">
+                {
+                    countries.map(country => <Country country={country} key={country.cca2}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
-    // console.log(country);
-    const {name,flags} = country;
+    console.log(country);
+    const {name,flags,area,population,cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = ()=>{
+        // setVisited(true);
+        setVisited(!visited);
+    }
     return (
         <div className="country">
+             <img src={flags.png} alt={name.common} width="100px" />
             <h3>Name:{name.common}</h3>
-            
-            <img src={flags.png} alt={name.common} width="100px" />
+            <p>area: {area}</p>
+            <p>Population: {population}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisited} className='btn-visited'>{visited ? 'Visited' : 'Click to visit'}</button>
+            {/* {visited && 'visited this countrt'} */}
+            {visited ? 'visited this country' : 'Click if you want to visit'}
+            {/* <p>Language: {languages.eng}</p> */}
            
         </div>
     );
