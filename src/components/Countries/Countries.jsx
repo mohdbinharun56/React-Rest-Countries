bb05d4feae0145ba04ffb70c1d7e96aa3f659f27
mohdbinharun56 <arahaman461@gmail.com>
2024-09-28 22:00:12 +0600

    display countries in a simple way with distructuring

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Country from "../Country/Country";
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -13,7 +14,10 @@ const Countries = () => {
     },[]);
     return (
         <div>
-            <h1>Countries</h1> 
+            <h1>Countries</h1>
+            {
+                countries.map(country=><Country country={country} key={country.cca2}></Country>)
+            } 
         </div>
     );
 };
